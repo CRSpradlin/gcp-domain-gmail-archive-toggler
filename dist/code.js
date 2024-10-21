252f13e88d283ff9a0686de337922448c38e20e3
crspradlin <github@crspradlin.org>
2024-10-20 20:46:13 -0400

    Dist folder updates for intial POC and toggleSwitch

--- dist/code.js
+++ dist/code.js
@@ -1,8 +1,10 @@
 function doGet(e) {}
 
+function GetEmailList() {}
+
 function SubmitNewEmailForm(formData) {}
 
-function ToggleEmail(formData) {}
+function ToggleEmail(email) {}
 
  /******/ (() => {
     // webpackBootstrap
@@ -26,10 +28,21 @@ function ToggleEmail(formData) {}
     var __webpack_exports__ = {};
     __webpack_require__.g.doGet = function(e) {
         return HtmlService.createHtmlOutputFromFile("dist/index.html").setSandboxMode(HtmlService.SandboxMode.IFRAME).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL).addMetaTag("viewport", "width=device-width, initial-scale=1").setTitle("GCPReactTemplate");
+    }, __webpack_require__.g.GetEmailList = function() {
+        return getExistingEmailData();
     }, __webpack_require__.g.SubmitNewEmailForm = function(formData) {
-        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, scriptProps = PropertiesService.getScriptProperties(), sheetId = String(scriptProps.getProperty("MAIN_SHEET_ID")), sheet = SpreadsheetApp.openById(sheetId).getSheets()[0], emailCountStr = scriptProps.getProperty("EMAIL_COUNT"), emailCount = 0;
+        var scriptProps = PropertiesService.getScriptProperties(), sheetId = String(scriptProps.getProperty("MAIN_SHEET_ID")), sheet = SpreadsheetApp.openById(sheetId).getSheets()[0], emailCountStr = scriptProps.getProperty("EMAIL_COUNT"), emailCount = 0;
         emailCountStr && (emailCount = parseInt(String(emailCountStr)));
-        var labels = null === (_c = null === (_b = null === (_a = Gmail.Users) || void 0 === _a ? void 0 : _a.Labels) || void 0 === _b ? void 0 : _b.list("me").labels) || void 0 === _c ? void 0 : _c.filter((function(label) {
+        var emailList = getExistingEmailData();
+        return console.log({
+            formData
+        }, [ formData.email, formData.archived && "on" == formData.archived ]), emailList.push([ formData.email, formData.archived && "on" == formData.archived ]), 
+        updateGmailFilter(emailList), sheet.getRange(emailCount + 1, 1).setValue(formData.email), 
+        sheet.getRange(emailCount + 1, 2).setValue(!(!formData.archived || "on" != formData.archived)), 
+        scriptProps.setProperty("EMAIL_COUNT", String(emailCount + 1)), emailList;
+    };
+    var updateGmailFilter = function(emailList) {
+        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, scriptProps = PropertiesService.getScriptProperties(), labels = null === (_c = null === (_b = null === (_a = Gmail.Users) || void 0 === _a ? void 0 : _a.Labels) || void 0 === _b ? void 0 : _b.list("me").labels) || void 0 === _c ? void 0 : _c.filter((function(label) {
             return "CSAutoArchived" == label.name;
         })), label = labels && labels.length > 0 ? labels[0] : undefined;
         label || (label = null === (_e = null === (_d = Gmail.Users) || void 0 === _d ? void 0 : _d.Labels) || void 0 === _e ? void 0 : _e.create({
@@ -40,14 +53,12 @@ function ToggleEmail(formData) {}
         scriptProps.deleteProperty("EMAIL_FILTER_ID")), console.log({
             filterIdStr
         });
-        var emailList = getExistingEmailData();
-        console.log({
-            formData
-        }, [ formData.email, formData.archived && "on" == formData.archived ]), emailList.push([ formData.email, formData.archived && "on" == formData.archived ]);
         var newFilter = null === (_l = null === (_k = null === (_j = Gmail.Users) || void 0 === _j ? void 0 : _j.Settings) || void 0 === _k ? void 0 : _k.Filters) || void 0 === _l ? void 0 : _l.create({
             criteria: {
-                to: "{".concat(emailList.map((function(emailRow) {
-                    return Boolean(emailRow[1]) ? emailRow[0] : undefined;
+                to: "{".concat(emailList.filter((function(emailRow) {
+                    return 1 == Boolean(emailRow[1]);
+                })).map((function(emailRow) {
+                    return emailRow[0];
                 })).join(","), "}")
             },
             action: {
@@ -57,23 +68,27 @@ function ToggleEmail(formData) {}
         }, "me");
         console.log({
             newFilter
-        }), scriptProps.setProperty("EMAIL_FILTER_ID", (null == newFilter ? void 0 : newFilter.id) || "Filter Created"), 
-        sheet.getRange(emailCount + 1, 1).setValue(formData.email), sheet.getRange(emailCount + 1, 2).setValue(formData.archived && "on" == formData.archived), 
-        scriptProps.setProperty("EMAIL_COUNT", String(emailCount + 1));
-    }, __webpack_require__.g.ToggleEmail = function(formData) {
-        return -1 != toggleEmail(formData.email);
+        }), scriptProps.setProperty("EMAIL_FILTER_ID", (null == newFilter ? void 0 : newFilter.id) || "Filter Created");
+    };
+    __webpack_require__.g.ToggleEmail = function(email) {
+        var response = toggleEmail(email);
+        return console.log({
+            toggleResponse: response
+        }), response;
     };
     var toggleEmail = function(emailToFind) {
-        var scriptProps = PropertiesService.getScriptProperties(), sheetId = String(scriptProps.getProperty("MAIN_SHEET_ID")), emailData = (SpreadsheetApp.openById(sheetId).getSheets()[0], 
-        getExistingEmailData());
+        var emailData = getExistingEmailData();
         for (var rowIndex in emailData) {
             var email = String(emailData[rowIndex][0]), toggled = Boolean(emailData[rowIndex][1]);
-            email == emailToFind && (emailData[rowIndex][1] = String(!toggled));
+            email == emailToFind && (emailData[rowIndex][1] = !toggled);
         }
-        return -1;
+        return updateGmailFilter(emailData), saveEmailData(emailData), emailData;
     }, getExistingEmailData = function() {
         var scriptProps = PropertiesService.getScriptProperties(), sheetId = String(scriptProps.getProperty("MAIN_SHEET_ID")), sheet = SpreadsheetApp.openById(sheetId).getSheets()[0], emailCount = parseInt(String(scriptProps.getProperty("EMAIL_COUNT")));
         return 0 == emailCount ? [] : sheet.getRange(1, 1, emailCount, 2).getValues();
+    }, saveEmailData = function(data) {
+        var scriptProps = PropertiesService.getScriptProperties(), sheetId = String(scriptProps.getProperty("MAIN_SHEET_ID")), sheet = SpreadsheetApp.openById(sheetId).getSheets()[0];
+        return scriptProps.setProperty("EMAIL_COUNT", data.length), 0 == data.length ? [] : sheet.getRange(1, 1, data.length, 2).setValues(data);
     };
     for (var i in __webpack_exports__) this[i] = __webpack_exports__[i];
     __webpack_exports__.__esModule && Object.defineProperty(this, "__esModule", {
